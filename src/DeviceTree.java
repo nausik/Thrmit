51ce1400391ef3f58ca02ef6eeb64fcbf3d23bca
FlyingPirate <vladborden@gmail.com>
2014-04-23 19:49:24 +0300

    Standard exception handling (database)

--- src/DeviceTree.java
+++ src/DeviceTree.java
@@ -2,4 +2,12 @@ public class DeviceTree {
 
 	private OneWireDevice[] devices;
 
+	public void setConnectionToDB(int port, String adress) {
+		try {
+			// sending some data
+		}
+		catch(SQLException exception) {
+			// handling this exception
+		}
+	}
 }
